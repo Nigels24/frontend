747c497f2139d1ee3b24065b2ee014926fe2528c
nigels24 <nigesipe@gmail.com>
2024-02-25 09:07:55 +0700

    Initiazlized Front-End

---
+++ src/components/AlumniSide/ABarComponent.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ABarComponent = () => {
+  return <div>ABarComponent</div>;
+};
+
+export default ABarComponent;
---
+++ src/configs/axios-base-url.jsx
@@ -0,0 +1,7 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "http://69.48.179.188:3001",
+});
+
+export default api;
